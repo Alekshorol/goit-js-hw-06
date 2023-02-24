435e34a1ccf24fbaa4449a180a76c2fd3f1f117f
AleksHor <horosh861@gmail.com>
2023-02-24 14:18:59 +0200

    done task second

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("item");
+  list.append(ingredientEl);
+  console.log(ingredientEl);
+  return ingredientEl;
+});
